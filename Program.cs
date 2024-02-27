f788785d579fdc046823e03360fdcb5e16873961
Özgür Sarıkaya <ozgursarikaya@gmail.com>
2024-02-27 22:25:37 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+Console.WriteLine("How many terms of the Fibonacci sequence would you like to see?");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Fibonacci Sequence:");
+for (int i = 0; i < n; i++)
+{
+    Console.Write(Fibonacci(i) + " ");
+}
+
+static int Fibonacci(int n)
+{
+    if (n <= 1)
+        return n;
+    else
+        return Fibonacci(n - 1) + Fibonacci(n - 2);
+}
